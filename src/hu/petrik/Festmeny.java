f8453ffc7c974960a14d95ea113cfd8aa49fbe32
Poós Dániel <poosdani@gmail.com>
2021-09-23 12:19:02 +0200

    Festmeny class függvényeinek leírása

--- src/hu/petrik/Festmeny.java
+++ src/hu/petrik/Festmeny.java
@@ -1,4 +1,62 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicit;
+    private boolean elkelt;
+
+    public Festmeny(String cim, String festo, String stilus){
+
+    }
+    public String getFesto() {
+        return this.festo;
+    }
+    public String getStilus() {
+        return this.stilus;
+    }
+    public int getLicitekSzama() {
+        return this.licitekSzama;
+    }
+    public int getLegmagasabbLicit() {
+        return this.legmagasabbLicit;
+    }
+    public LocalDateTime getLegutolsoLicit() {
+        return this.legutolsoLicit;
+    }
+    public boolean getElkelt() {
+        return this.elkelt;
+    }
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+    public void licit(){
+        if (getElkelt()){
+            System.out.println("A festmény már elkelt T-T ");
+        }
+        if (getLicitekSzama() == 0){
+            this.legmagasabbLicit += 100;
+            this.licitekSzama++;
+            this.legutolsoLicit = LocalDateTime.now();
+        }
+        if (getLicitekSzama() > 0){
+            licit(10);
+        }
+    }
+    public void licit(int mertek){
+        if (10<=mertek && mertek<=100){
+            this.legmagasabbLicit += this.legmagasabbLicit * (mertek/10);
+            this.licitekSzama++;
+            this.legutolsoLicit = LocalDateTime.now();
+        }
+        else{
+            System.out.println("Nem megfelelő mérték");
+        }
+    }
+
 }
